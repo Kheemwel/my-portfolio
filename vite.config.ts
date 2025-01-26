ab79cb1a1a643c56dfae635149beedadf939987c
Kimwel Beller <94892701+Kheemwel@users.noreply.github.com>
2025-01-26 20:50:47 +0800

    refactor: remove base in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/my-portfolio/',
   plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
